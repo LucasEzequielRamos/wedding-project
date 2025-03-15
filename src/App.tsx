a157b037108775aa7003b9aeaa390ea0f7791e7a
Lucas Becerra <lucaseramos13@gmail.com>
2025-03-15 01:23:22 -0300

    set some variables

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-accent">
+        <p className="text-6xl text-primary">Hola</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
