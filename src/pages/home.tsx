d34682c7e55a4a4d9cac54610f98bcea5c4a71a1
Lucas Becerra <lucaseramos13@gmail.com>
2025-08-26 06:30:14 -0300

    refactor(home): remove unnecessary import of useEffect

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,7 +9,6 @@ import Faq from "../sections/faq";
 import Footer from "../sections/footer";
 import MiniHero from "../sections/mini-hero";
 import ToGifts from "../components/to-gifts";
-import { useEffect } from "react";
 
 function Home() {
   return (
